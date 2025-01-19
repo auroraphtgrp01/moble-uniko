54240e7dc5cd28a40e2a1f2e21fc74a08b135dc0
Minh Triet Pham <phamminhtriet0312@gmail.com>
2025-01-19 13:57:35 +0700

    feat: sign in with google

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,4 +1,6 @@
 import 'dart:convert';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:http/http.dart' as http;
 import '../config/app_config.dart';
 import 'package:local_auth/local_auth.dart';
@@ -30,6 +32,21 @@ class AuthService {
     } catch (e) {
       return false;
     }
+  }
+
+  // Google Sign In
+  signInWithGoogle() async {
+// begin interactive sign in process
+    final GoogleSignInAccount? gUser = await GoogleSignIn().signIn();
+// obtaiin auth details from request
+    final GoogleSignInAuthentication gAuth = await gUser!.authentication;
+// create a new credential for user
+    final credential = GoogleAuthProvider.credential(
+      accessToken: gAuth.accessToken,
+      idToken: gAuth.idToken,
+    );
+// finally, lets sign in
+    return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 
   // Login
